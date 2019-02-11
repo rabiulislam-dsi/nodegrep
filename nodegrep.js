67ef28b52af2e2953476143166528d4c42e37afb
Rabiul Islam Anik <rabiul.islam2676@gmail.com>
2019-02-11 13:11:11 +0600

    comments out the fs-implementation

--- nodegrep.js
+++ nodegrep.js
@@ -5,25 +5,24 @@ let queryArgs = process.argv.slice(2);
 let queryWord = queryArgs[0];
 let queryFile = queryArgs[1];
 
-fs.readFile(`./${queryFile}`, (err, buffer) => {
-  if (err) throw err;
-  //console.log(data);
-  let bufferString = buffer.toString();
-  let matchedLines = bufferString
-    .split("\n")
-    .filter((line, index) =>
-      line.toLowerCase().includes(queryWord.toLowerCase())
-    );
-  console.log(matchedLines.join("\n"));
+// fs.readFile(`./${queryFile}`, (err, buffer) => {
+//   if (err) throw err;
+//   //console.log(data);
+//   let bufferString = buffer.toString();
+//   let matchedLines = bufferString
+//     .split("\n")
+//     .filter((line, index) =>
+//       line.toLowerCase().includes(queryWord.toLowerCase())
+//     );
+//   console.log(matchedLines.join("\n"));
+// });
+
+let readlineInterFace = readline.createInterface({
+  input: fs.createReadStream(`./${queryFile}`)
 });
 
-// let rd = readline.createInterface({
-//     input: fs.createReadStream('./demo1.txt')
-// });
-//
-// rd.on('line', (line)=> {
-//   console.log('Got another line...');
-//   if(line.includes('line')){
-//       console.log(line);
-//   }
-// });
+readlineInterFace.on("line", line => {
+  if (line.includes(queryWord)) {
+    console.log(line);
+  }
+});
